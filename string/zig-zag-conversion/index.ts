d1a19d72aaf9a2738762882c69c966d6e7fbf167
Nirmal <nirmal@keyvalue.systems>
2024-01-29 15:48:05 +0530

    create: zig-zag-conversion

---
+++ string/zig-zag-conversion/index.ts
@@ -0,0 +1,32 @@
+function convert(s: string, numRows: number): string {
+  const stringArray = new Array(numRows).fill("");
+  let index = 0;
+  let direction = "forward";
+  let currentStringIndex = 0;
+
+  while (index < s.length) {
+    stringArray[currentStringIndex] += s.charAt(index);
+    if (direction === "forward") {
+      if (currentStringIndex === numRows - 1) {
+        direction = "backward";
+        currentStringIndex--;
+      } else {
+        currentStringIndex++;
+      }
+    } else {
+      if (currentStringIndex === 0) {
+        direction = "forward";
+        currentStringIndex++;
+      } else {
+        currentStringIndex--;
+      }
+    }
+
+    index++;
+  }
+
+  return stringArray.join("");
+}
+
+const result = convert("PAYPALISHIRING", 4); // expected result 
+console.log(result);
